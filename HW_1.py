7054d77a8dc92a266fb8175cbe7345ad974130cf
Veronika Rouba <Roubaveronika@gmail.com>
2021-09-12 00:16:44 +0300

    first commit

---
+++ HW_1.py
@@ -0,0 +1,57 @@
+# Python. HW_1
+#
+# 1) Создайте переменную типа String
+Name_1 = "Veronika"
+# 2) Создайте переменную типа Integer
+Name_2 = 31
+# 3) Создайте переменную типа Float
+Name_3 = 31.3
+# 4) Создайте переменную типа Bytes
+Name_4 = b'hello!'
+# 5) Создайте переменную типа List
+Name_5 = ['a', 'b', 'c']
+# 6) Создайте переменную типа Tuple
+Name_6 = ('d', )
+# 7) Создайте переменную типа Set
+Name_7 = set('hello')
+# 8. Создайте переменную типа Frozen set.
+Name_8 = frozenset('hello')
+# 9) Создайте переменную типа Dict
+Name_9 = {1: 2, 2: 4}
+# 10) Вывести в консоль все перечисленные выше переменные с добавлением типа данных.
+print(Name_1, type(Name_1))
+print(Name_2, type(Name_2))
+print(Name_3, type(Name_3))
+print(Name_4, type(Name_4))
+print(Name_5, type(Name_5))
+print(Name_6, type(Name_6))
+print(Name_7, type(Name_7))
+print(Name_8, type(Name_8))
+print(Name_9, type(Name_9))
+
+# 11) Создайте 2 строковые переменные, создайте переменную, в которой вы суммируете эти переменные. Вывод в консоль.
+Name_10 = 'Bye'
+Name_11 = 'Bye'
+Name_plus = Name_10 + Name_11
+print(Name_plus)
+# 12) Создайте 2 целочисленные переменные, создайте переменную, в которой вы суммируете эти переменные. Вывод в консоль.
+Name_12 = 10
+Name_13 = 2
+Name_plus2 = Name_12 + Name_13
+print(int(Name_plus2))
+# 13) Создайте переменную, в которой разделите эти переменные Int. Вывод в консоль.
+Name_division = Name_12 / Name_13
+print(int(Name_division))
+# 14) Создайте переменную, в которой умножьте эти переменные Int. Вывод в консоль.
+Name_multi = Name_12 * Name_13
+print(int(Name_multi))
+# 15) Создайте переменную, в которой без остатка отделяют эти переменные типа Int. Вывод в консоль.
+Name_a = 8
+Name_b = 3
+Name_15 = Name_a // Name_b
+print(int(Name_15))
+# 16) Создайте переменную, в которой вы хотите присвоить остаток от деления этих переменных Int. Вывод в консоль.
+Name_a = 7
+Name_b = 3
+Name_16 = Name_a % Name_b
+print(int(Name_16))
